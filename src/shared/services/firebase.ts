20ca03f5a2843f5988eaa7e97da6554fcac2063b
Aristidescosta <aristidiscosta200@gmail.com>
2024-10-05 15:28:13 +0100

    feat: adicionando o .env.example

--- src/shared/services/firebase.ts
+++ src/shared/services/firebase.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAtkWvUqfjgGVkZ4Kw6TtZrXsQk4a2_V2s",
-    authDomain: "to-do-list-c0cab.firebaseapp.com",
-    projectId: "to-do-list-c0cab",
-    storageBucket: "to-do-list-c0cab.appspot.com",
-    messagingSenderId: "256957965874",
-    appId: "1:256957965874:web:3bf950ab61c25e6ac1be2b",
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
